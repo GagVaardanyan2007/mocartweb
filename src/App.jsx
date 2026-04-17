518f2c9bd2774b588169c44203ce0f4a26f7fa2a
GagVaardanyan2007 <foxikmopzzi@gmail.com>
2026-04-17 16:08:35 +0400

    f

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { herodata } from '../data/herdata'
 import './App.css'
 import Conteiners from './components/Conteiners'
+
 import Hero from './components/homepage/Hero'
 
 
--- src/components/homepage/AboutInfo.jsx
+++ src/components/homepage/AboutInfo.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 export default function AboutInfo( { data } ) {
   return (
-    <div className='w-full bg-amber-800 border-amber-500 text-white flex p-5 border-3 rounded-xl shadow-2xl flex-col '>
+    <div className='w-full bg-[#593312] border-amber-500 text-white flex p-5 border-3 rounded-xl shadow-2xl flex-col '>
       <h1 className='text-center text-4xl'>About Us</h1>
-      <div className='flex justify-between items-center mt-10 gap-5'>
-        <img src={data.image} alt="" className='w-100' />
-        <div className="w-1/2">{data.description}</div>
+      <div className='flex mt-10 gap-5'>
+        <div className='border rounded-2xl  overflow-hidden border-gray-300 w-100  shadow-lg'>
+            <img src={data.image} alt="" className='hover:scale-105 transition-transform duration-300' />
+        </div>
+        <div className="w-full">{data.description}</div>
       </div>
     </div>
   )
