d2b3c18dc4e84d8b70c3286f5d163a4d260e6837
GagVaardanyan2007 <foxikmopzzi@gmail.com>
2026-04-17 15:58:32 +0400

    t

--- src/components/homepage/AboutInfo.jsx
+++ src/components/homepage/AboutInfo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function AboutInfo( { data } ) {
   return (
-    <div className='w-full bg-black/70 text-white flex p-5 border-2 rounded-xl shadow-2xl flex-col border-white'>
+    <div className='w-full bg-amber-800 border-amber-500 text-white flex p-5 border-3 rounded-xl shadow-2xl flex-col '>
       <h1 className='text-center text-4xl'>About Us</h1>
       <div className='flex justify-between items-center mt-10 gap-5'>
         <img src={data.image} alt="" className='w-100' />
--- src/components/homepage/Hero.jsx
+++ src/components/homepage/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Hero({ data }) {
   return (
     <div className='w-full h-screen bg-no-repeat bg-cover flex justify-center items-center ' style={{ backgroundImage: data.heroimg }} >
-        <div cls>
+        <div className='w-1/2 '>
             <h1 className='text-6xl text-white font-bold text-center pt-40'>{data.herotitle}</h1>
             <p className='text-center text-white mt-5 text-xl'>{data.herodesc}</p>
             <div className='flex justify-center mt-10'>
