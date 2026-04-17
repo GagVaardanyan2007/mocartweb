1fd0c01258b0b0f9237d4968a58145b0f1ace6fb
GagVaardanyan2007 <foxikmopzzi@gmail.com>
2026-04-17 16:17:43 +0400

    f

--- src/components/homepage/AboutInfo.jsx
+++ src/components/homepage/AboutInfo.jsx
@@ -2,13 +2,18 @@ import React from 'react'
 
 export default function AboutInfo( { data } ) {
   return (
-    <div className='w-full bg-[#593312] border-amber-500 text-white flex p-5 border-3 rounded-xl shadow-2xl flex-col '>
+    <div className='w-full bg-[#40240b] border-amber-500 text-white flex p-5 border-3 rounded-xl shadow-2xl flex-col '>
       <h1 className='text-center text-4xl'>About Us</h1>
-      <div className='flex mt-10 gap-5'>
+      <div className='flex mt-10 gap-25'>
         <div className='border rounded-2xl  overflow-hidden border-gray-300 w-100  shadow-lg'>
             <img src={data.image} alt="" className='hover:scale-105 transition-transform duration-300' />
         </div>
-        <div className="w-full">{data.description}</div>
+        <div className="w-full text-justify">{data.description}</div>
+      </div>
+      <div>
+        <div>
+            
+        </div>
       </div>
     </div>
   )
